c94c06897f99682efead044d9f4f7116ec02bfae
Feanaur <feanaur2193@gmail.com>
2013-12-31 15:42:10 +0700

    Added: delete comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ RailsBlog::Application.routes.draw do
   resources :posts do
     resources :comments
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root to: "welcome#index"
 
   # Example of regular route:
