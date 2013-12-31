36f0d954493899e3f6de9f5b68757a1fad041a70
Feanaur <feanaur2193@gmail.com>
2013-12-31 15:26:34 +0700

    User and Comment models added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131230091947) do
+ActiveRecord::Schema.define(version: 20131231082502) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,5 +29,17 @@ ActiveRecord::Schema.define(version: 20131230091947) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "post_id"
+    t.integer  "comment_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["comment_id"], name: "index_users_on_comment_id"
+  add_index "users", ["post_id"], name: "index_users_on_post_id"
 
 end
