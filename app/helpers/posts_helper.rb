caa10472ba81753fe21114ea582decd2b798ce85
Feanaur <feanaur2193@gmail.com>
2014-01-09 10:46:25 +0700

    Preview of post helper and correct listing of posts added

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+
+  def post_preview(post)
+    preview = ""
+    unless post==nil
+      i=0
+      until i>100 && post.text[i-1] == "."
+        if i<post.text.length
+          preview += post.text[i]
+          i+=1
+        else
+          break
+        end
+      end
+    end
+    preview
+  end
+
+  def pretty_date(time)
+    time.strftime("%d %b %Y")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3 }
-  validates :body, presence: true
+  validates :text, presence: true
   belongs_to :user
   has_many :comments, :dependent => :destroy
 end
